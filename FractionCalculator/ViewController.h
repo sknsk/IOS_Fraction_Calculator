074d72e6e17d01f2ae9f701cc283b82aea712e6b
F839-019 M <mf839-019@mf839-019.local>
2016-05-16 14:40:15 +0900

    Initial Commit

---
+++ FractionCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FractionCalculator
+//
+//  Created by MF839-019 on 2016. 5. 16..
+//  Copyright © 2016년 MF839-019. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FractionCalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FractionCalculator
+//
+//  Created by MF839-019 on 2016. 5. 16..
+//  Copyright © 2016년 MF839-019. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
